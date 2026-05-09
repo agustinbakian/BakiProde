08e545a1d3bb9799c19fa8f5c674825cb219c6d2
agustinbakian <agustin@bakian.io>
2026-05-09 04:23:55 -0300

    Update Flag.jsx

--- bakiprode/src/components/Flag.jsx
+++ bakiprode/src/components/Flag.jsx
@@ -1,15 +1,14 @@
 import { FLAG_ISO } from "../lib/fixture";
 
-export function Flag({ country, size = 24 }) {
+export function Flag({ country }) {
   const code = FLAG_ISO[country];
   if (!code) return null;
-  const h = Math.round(size * 0.75);
   return (
     <img
-      src={`https://flagcdn.com/${size}x${h}/${code}.png`}
-      srcSet={`https://flagcdn.com/${size * 2}x${h * 2}/${code}.png 2x`}
-      width={size}
-      height={h}
+      src={`https://flagcdn.com/24x18/${code}.png`}
+      srcSet={`https://flagcdn.com/48x36/${code}.png 2x`}
+      width={24}
+      height={18}
       alt={country}
       style={{ borderRadius: 2, objectFit: "cover", border: "0.5px solid rgba(0,0,0,0.1)", flexShrink: 0 }}
     />
