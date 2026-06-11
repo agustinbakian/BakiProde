0c9ff2bd3fccc3060aee355a40507f931b865e65
agustinbakian <agustin@bakian.io>
2026-06-11 18:45:02 -0300

    Update resultados.js

--- bakiprode/api/resultados.js
+++ bakiprode/api/resultados.js
@@ -2,12 +2,12 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET");
 
-  const API_KEY = process.env.FOOTBALL_DATA_API_KEY;
+  const API_KEY = process.env.WORLDCUP_API_KEY;
 
   try {
     const response = await fetch(
-      "https://api.football-data.org/v4/competitions/2000/matches?season=2026",
-      { headers: { "X-Auth-Token": API_KEY } }
+      "https://worldcupapi.com/api/matches",
+      { headers: { "Authorization": `Bearer ${API_KEY}` } }
     );
     const data = await response.json();
     res.status(200).json(data);
